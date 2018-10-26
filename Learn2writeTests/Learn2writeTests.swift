5b6483b9a4d32b52d72a4eea68ccb5c8985f8b6b
Yihai Tian <zx.james@gmail.com>
2018-10-25 18:17:49 -0600

    Initial Commit

---
+++ Learn2writeTests/Learn2writeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Learn2writeTests.swift
+//  Learn2writeTests
+//
+//  Created by Yihai Tian on 2018-10-25.
+//  Copyright © 2018 Yihai Tian. All rights reserved.
+//
+
+import XCTest
+@testable import Learn2write
+
+class Learn2writeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
